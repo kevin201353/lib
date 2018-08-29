fb1326f468a83fe63e3c8bf0669c715663fe34d2
kevin201353 <kevin_201353@126.com>
2018-08-29 16:25:00 +0800

    update go tcp

--- go/src/main/main.go
+++ go/src/main/main.go
@@ -3,9 +3,36 @@ package main
 import (
 	"math2"
 	"fmt"
+	"net"
 )
+
+func handleConnection(conn net.Conn)  {
+	defer conn.Close()
+	for {
+		data := make([]byte, 1024)
+		n, err := conn.Read(data)
+		if n == 0 || err != nil {
+			break
+		}
+		fmt.Printf("Read len: %d,  data: %s", n, data)
+		conn.Write(data)
+	}
+}
 
 func main() {
 	b := math2.Sum(2,3)
 	fmt.Println("Hello,world!, b:", b)
+	ln, err := net.Listen("tcp",  "localhost:8081")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handleConnection(conn)
+	}
+
 }
